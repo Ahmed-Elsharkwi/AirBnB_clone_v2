724fbe6b144cd66015d175f640f1ad505cfc1290
Ahmed <ahmedelsharkwiti@gmail.com>
2024-02-04 00:02:10 +0200

    02-04 00:02

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 """
- script (based on the file 1-pack_web_static.py) that distributes an 
- archive to your web servers, using the function do_deploy
- """
-from fabric.api import run, env, put
-import os.path
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['100.25.134.180', '18.210.16.180']
+
 
 def do_deploy(archive_path):
-    """ deploy remote servers """
-    if os.path.exists(archive_path) is False:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split('/')[-1]
-        file_no_ext = file_name.split('.')[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-
-        env.hosts = ['100.25.134.180', '18.210.16.180']
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
-        run('rm -rf {}{}/web_static'.format(path, file_no_ext))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
-
