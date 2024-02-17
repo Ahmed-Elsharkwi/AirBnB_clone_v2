ae0af74563e40717d4b9d573dd5cd15dbc07cf9f
Ahmed <ahmedelsharkwiti@gmail.com>
2024-02-17 17:20:34 +0200

    02-17 17:20

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Web server
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+
+    """ print Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def show():
+    """ print HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """ print C <text> """
+    return "C {}".format(text.replace('_', " "))
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text="is cool"):
+    """ print python <text> """
+    return "python {}".format(text.replace('_', " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
